19feb0dc3d63e55f1d85fd6cb253bf8fdb63a46e
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-04-22 11:17:32 +0900

    GItHub作成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,10 @@
 #include <Windows.h>
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) 
+{
+
+
 	
 	return 0;
 }
