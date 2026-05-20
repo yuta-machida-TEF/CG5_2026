1dd2352a1b956579df129a16dae1def970c9fefd
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-05-20 12:20:03 +0900

    CG5_00-02

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -65,12 +65,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
-	
-	//// コンパイル
-	//ID3DBlob* vsBlob = nullptr;
-	//ID3DBlob* psBlob = nullptr;
-	//ID3DBlob* errorBlob = nullptr;
-
 	//頂点シェーダー
 	ID3DBlob* vsBlob = CompileShader(L"Resources/shaders/TestVS.hlsl","vs_5_0");
 	assert(vsBlob != nullptr);
@@ -79,44 +73,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	ID3DBlob* psBlob = CompileShader(L"Resources/shaders/TestPS.hlsl","ps_5_0");
 	assert(psBlob != nullptr);
 
-
-	//hr = D3DCompileFromFile(
-	//	vsFile.c_str(), 
-	//	nullptr,
-	//	D3D_COMPILE_STANDARD_FILE_INCLUDE,
-	//	"main", "vs_5_0", 
-	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
-	//	0, &vsBlob, &errorBlob);
-
-	//if (FAILED(hr)) 
-	//{
-	//	DebugText::GetInstance()->ConsolePrintf(
-	//		std::system_category().message(hr).c_str());
-	//	if (errorBlob)
-	//	{
-	//		DebugText::GetInstance()->ConsolePrintf(
-	//			reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
-	//	}
-	//	assert(false);
-	//}
-
-	//hr = D3DCompileFromFile(
-	//	psFile.c_str(),//シェーダーファイル名
-	//	nullptr,
-	//	D3D_COMPILE_STANDARD_FILE_INCLUDE,//インクルード可能にする
-	//	"main", "ps_5_0", //エントリーポイント名、シェーダーモデル指定
-	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,//デバック用設定
-	//	0, &psBlob, &errorBlob);
-	//if (FAILED(hr)) {
-	//	DebugText::GetInstance()->ConsolePrintf(
-	//		std::system_category().message(hr).c_str());
-	//	if (errorBlob){
-	//		DebugText::GetInstance()->ConsolePrintf(
-	//			reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
-	//	}
-	//	assert(false);
-
-	//}
 
 	//PSO
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStaticDesc{};
@@ -211,9 +167,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	vertexResource->Release();
 	graphicsPipeLineState->Release();
 	signatureBlob->Release();
-	/*if (errorBlob) {
-		errorBlob->Release();
-	}*/
 	rootSignature->Release();
 	vsBlob->Release();
 	psBlob->Release();
