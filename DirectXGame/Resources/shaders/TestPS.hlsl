934e212bb2ed5433840969cd16a1a0f5c73fdd3d
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-07-16 14:02:55 +0900

    CG5_00-10のrabをrgbに修正

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -16,7 +16,7 @@ PixelShaderOutput main(VertexShaderOutput input)
 
     
     //grayscale
-    float32_t value = dot(textureColor.rab, float32_t3(0.2125f,0.7154f,0.0721f));
+    float32_t value = dot(textureColor.rgb, float32_t3(0.2125f,0.7154f,0.0721f));
     output.color = float32_t4(value,value,value,textureColor.a);
 
    return output;
